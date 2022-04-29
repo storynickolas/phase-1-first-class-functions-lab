d3dafae99a486ac542f36d4969bbe029b37edf15
Nick Story <storynickolasl@gmail.com>
2022-04-29 08:13:09 -0400

    commit tree e48f97e1daa515bb32184ef267a0a54c482458c8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2)
+}
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ]
+
+function createFareMultiplier(num1) {
+  return function(num2) { return num1 * num2}
+}
+
+
+const fareDoubler = (num3) => {
+  const double = createFareMultiplier(2)
+  return double(num3)
+}
+
+const fareTripler = (num4) => {
+  const Triple = createFareMultiplier(3)
+  return Triple(num4)
+}
+
+const selectDifferentDrivers = (driversArray, driverFunction) => driverFunction(driversArray)
